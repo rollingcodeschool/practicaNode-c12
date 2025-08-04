4b018587d3edf92336b062aba478ee708f26527c
Emilse Arias <emilsearias@gmail.com>
2025-08-04 11:10:47 -0300

    agregue una opcion de trabajar con modulos usando commonjs y esmodules

---
+++ src/5-operacionesMatematicas.js
@@ -0,0 +1,7 @@
+export function multiplicar(a,b){
+    return a * b
+}
+
+export function dividir(a,b){
+    return a/b;
+}
---
+++ src/6-mainESmodules.js
@@ -0,0 +1,4 @@
+import { dividir, multiplicar } from "./5-operacionesMatematicas.js";
+
+console.log(`Dividir: ${dividir(20,3)}`)
+console.log(`Multiplicar: ${multiplicar(13,3)}`)
