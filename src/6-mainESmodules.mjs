e8aba84c65681232e6599a6994b52b1728c8b86f
Emilse Arias <emilsearias@gmail.com>
2025-08-04 11:14:25 -0300

    mostramos como funcionan los esmodules usando la extension .mjs

--- src/6-mainESmodules.mjs
+++ src/6-mainESmodules.mjs
@@ -1,4 +1,4 @@
-import { dividir, multiplicar } from "./5-operacionesMatematicas.js";
+import { dividir, multiplicar } from "./5-operacionesMatematicas.mjs";
 
 console.log(`Dividir: ${dividir(20,3)}`)
 console.log(`Multiplicar: ${multiplicar(13,3)}`)
